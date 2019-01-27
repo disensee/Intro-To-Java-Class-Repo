8ace5df7b36b45854a337091937618cb80792304
disensee <isenseed@students.westerntc.edu>
2019-01-27 17:07:34 -0600

    Added SampleCodeTest class to test code snippets

---
+++ Java Sample Code/src/com/dylanisensee/samplecode/SampleCodeTest.java
@@ -0,0 +1,12 @@
+package com.dylanisensee.samplecode;
+
+public class SampleCodeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	char letter = 65;
+	String foo = "hello";
+	System.out.println(foo);
+	}
+
+}
