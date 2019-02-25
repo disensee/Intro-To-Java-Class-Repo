11b20ef7a126df2b58850022c915890ab6aa265b
disensee <isenseed@students.westerntc.edu>
2019-02-24 19:28:46 -0600

    Worked on ArrayProblems2

--- Java Sample Code/src/com/dylanisensee/samplecode/ArrayProblemsPart2.java
+++ Java Sample Code/src/com/dylanisensee/samplecode/ArrayProblemsPart2.java
@@ -1,5 +1,7 @@
 package com.dylanisensee.samplecode;
 
+import java.util.Arrays;
+
 public class ArrayProblemsPart2 {
 
 	public static void main(String[] args) {
@@ -15,16 +17,30 @@ public class ArrayProblemsPart2 {
 		// Variables for each part have been declared for you (protocol, hostName, fileName), you need to figure out how to initialize them.
 		// Sysout the variables when you are done
 		String url = "https://www.google.com/somefolder/someotherfolder/somefile.html";
-		String protocol,hostName,fileName;
+		String[] protocolSplit = url.split(":");
+		String protocol = protocolSplit[0];
+		
+		String[] urlSplit = url.split("/");
+		String hostName = urlSplit[2];
+		String fileName = urlSplit[5];
+		
+		System.out.println(protocol);
+		System.out.println(hostName);
+		System.out.println(fileName);
 		
 		
 		
 		// Problem 2 - Swap 2 elements in an array
-		// Write code swaps the first 2 elements in the 'someNumbers' array below.
+		// Write code that swaps the first 2 elements in the 'someNumbers' array below.
 		// When you are done the array should look like this: {1,2,3,4,5}
 		// When you are done, loop through the array and sysout each value so that you can verify that your code worked
 		int[] someNumbers = {2,1,3,4,5};
-		
+		int temp = someNumbers[1];
+		someNumbers[1] = someNumbers[0];
+		someNumbers [0] = temp;
+		for(int i = 0; i < someNumbers.length; i++) {
+			System.out.println(someNumbers[i]);
+		}
 		
 		
 		// Problem 3 - Nested Loops
@@ -41,7 +57,13 @@ public class ArrayProblemsPart2 {
 		// This will return an array of words within the line.
 		// Add a nested loop that iterates through each word in the current line.
 		// Inside the body of the nested loop, simply sysout each word.
-		
+		String[] lineSplit = poem.split("\n");
+		for(int i = 0; i < lineSplit.length; i++) {
+			String[] wordSplit = lineSplit[i].split(" ");
+			for(int x = 0; x < wordSplit.length; x++) {
+				System.out.println(wordSplit[x]);
+			}	
+		}
 		
 		
 		
@@ -55,7 +77,7 @@ public class ArrayProblemsPart2 {
 		// If you don't find the value within the array then return -1 (which is an invalid index number).
 		// Once you finish writing the code for the body of the method you can use this next bit of code to test it.
 		
-		/*
+		
 		// this code will test the indexOfElement() method
 		int[] scores = {88,74,100,83};
 		int perfectScore = 100;
@@ -64,7 +86,7 @@ public class ArrayProblemsPart2 {
 		int worstScore = 0;
 		index = ArrayProblemsPart2.indexOfElement(scores, worstScore);
 		System.out.println(index); // this should sysout -1
-		*/
+		
 		
 	
 		// Problem 5
@@ -77,17 +99,17 @@ public class ArrayProblemsPart2 {
 		// Then, you'll have to add the value from the elementToAdd param to the new array.
 		// Once you finish writing the body of the method, you can use the next bit of code to test it.
 				
-		/*
+		
 		// This code will test the addElement() method
-		int[] scores = {88, 93, 72}; // here's the original array
+		int[] scores2 = {88, 93, 72}; // here's the original array
 		int scoreToAdd = 100; // here's the value we want to add to the array
-		scores = ArrayProblemsPart2.addElement(scores, scoreToAdd); // invoke the addElement() method like this, and assign the return value to the scores variable
+		scores2 = ArrayProblemsPart2.addElement(scores2, scoreToAdd); // invoke the addElement() method like this, and assign the return value to the scores variable
 		
 		// let's have a look at what's in the scores array now...
-		for(int s : scores) {
+		for(int s : scores2) {
 			System.out.println(s);
 		}
-		*/
+		
 		
 		// Problem 6
 		// Write code that identifies duplicate values in an array by creating a new array that contains the duplicate values
@@ -96,25 +118,22 @@ public class ArrayProblemsPart2 {
 		// Finally, sysout each value in the new array
 		int[] numbers = {1,2,5,1,3,7,5,8,5,9,9,10};
 		
-				
-
 	}
 	
 	
 	public static int indexOfElement(int[] arrayToSearch, int valueToSearchFor) {
-		
-		// Comment out this next line before you start adding code for this method (it just prevents a compiler error)
-		return 0;
-			
+		for(int i = 0; i < arrayToSearch.length; i ++) {
+			if(arrayToSearch[i] == valueToSearchFor) 
+				return i;	
+			}
+		return -1;	
 	}
 	
 	
 	public static int[] addElement(int[] originalArray, int elementToAdd) {
-		
-		// Comment out this next line before you start adding the code for this method (it just creates and returns an empty array to avoid compiler error)
-		return new int[0];
-				
+		int[] newArray = Arrays.copyOf(originalArray, originalArray.length + 1);
+		newArray[newArray.length - 1] = elementToAdd;
+		return newArray;				
 	}
 	
-
 }
