d488a5d572e5e850f9d0f3eb4f4966b4f606ceec
disensee <isenseed@students.westerntc.edu>
2019-03-06 21:00:40 -0600

    Added rectangle class from code copy assignment

---
+++ Java Sample Code/src/com/dylanisensee/samplecode/Rectangle.java
@@ -0,0 +1,17 @@
+package com.dylanisensee.samplecode;
+
+public class Rectangle {
+	private int width;
+	private int height;
+	private int area;
+	
+	public void setDimensions(int newWidth, int newHeight) {
+		width = newWidth;
+		height = newHeight;
+		area = width * height;
+	}
+	
+	public int getArea() {
+		return area;
+	}
+}
